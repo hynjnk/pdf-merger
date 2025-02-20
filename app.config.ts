be6b597dbb586555fe78a338f5963c74a9a35788
Hyunjoon KIM <hyunjoon@live.com>
2025-02-20 12:44:08 +0900

    update config for SPA output

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,9 @@
 import { defineConfig } from "@solidjs/start/config";
 
-export default defineConfig({});
+export default defineConfig({
+    // https://github.com/solidjs/solid-start/discussions/1398#discussioncomment-8833738
+    ssr: false,
+    server: {
+        static: true
+    }
+});
